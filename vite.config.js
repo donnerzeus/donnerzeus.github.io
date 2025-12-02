1c02d3335baa86be00a29408c5e558f72d22ba86
Barış Delikaya <barisdelikaya@Bars-MacBook-Pro.local>
2025-12-02 15:56:25 +0300

    docs targeted

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./",   // <<< GitHub Pages için zorunlu
+  build: {
+    outDir: 'docs',
+  }
 })
